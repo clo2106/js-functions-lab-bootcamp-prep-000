64ae6728eccc7e8facd67ea092368cc0bd00af60
Christopher Lopes <chrislopes1091@gmail.com>
2017-12-05 01:30:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// write your code below!
+function happyHolidays(){
+  return 'Happy holidays!'
+  var happyHolidays = "Happy holidays!"
+}
+
+  function happyHolidaysTo(name){
+    return 'Happy holidays, you!'
+    var name = 'Happy holidays, you!'
+  };
+
+ function happyHolidaysTo(holiday, name){
+   var name() = 'you'
+   var holiday() = ' Independence day'
+
+
+ }
